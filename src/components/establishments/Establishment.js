a06f5df3afdc4baa59a3eab02a3ed6b20b67c58d
Dejaiffe Christophe <cdejaiffe@gmail.com>
2019-08-05 16:38:53 +0200

    Favoris bouton

--- src/components/establishments/Establishment.js
+++ src/components/establishments/Establishment.js
@@ -1,9 +1,8 @@
 import React, { Component, Fragment } from 'react';
 import Like from './../../assets/Like.png'; // import des logos
 import DisLike from './../../assets/DisLike.png'; 
-import Favoris1 from './../../assets/Fav01.png';
-import Favoris2 from './../../assets/Fav02.png';
-
+import Logo1 from './../../assets/Fav01.png';
+import Logo2 from './../../assets/Fav02.png';
 
 class Establishment extends Component {
 
@@ -12,7 +11,7 @@ class Establishment extends Component {
         this.state = {
             countLik: 0,
             countDis: 0,
-            Favori: Favoris1
+            favori: false
         };
 }
 add() { // Je modifie le state pour incrémenter le compteur de 1 ( compteur like)
@@ -21,31 +20,43 @@ add() { // Je modifie le state pour incrémenter le compteur de 1 ( compteur lik
     });
 }
     
-min() { // Je modifie le state pour incrémenter le compteur de 1 ( compteur like)
+min() { // Je modifie le state pour incrémenter le compteur de 1 ( compteur dislike)
     this.setState({
         countDis: this.state.countDis +1
     });
 
 }
-fav() {
+favori = () => {
     this.setState({
-        Favori : this.state.Favori.Favoris2
+        favori : !this.state.favori
     })
     
 }
 
+
     render() { // Fragment car return de plusieurs éléments
+
+        let starIcon    = <i className="fa fa-star-o" aria-hidden="true"></i>
+
+        if (this.state.favori) {
+            starIcon = <i className="fa fa-star favoriIcon" aria-hidden="true"></i>
+        }
 
         return (
             <Fragment> 
             <div className='establishment'>
+            <div className='establishment-favori' >
+                <button onClick={this.favori}>{ starIcon }</button>
+            </div>
                 <h3>{ this.props.establishment.name }</h3> 
-
                     { this.props.establishment.description }
-                <div className= 'Button'>
-                <p><button onClick={() => this.fav()}><img src={Favoris1} alt="Favoris" /></button> </p>
-                </div>   
+                <div>
+                    <p> <span> { this.state.favori} </span> </p>
+                </div>
+                
+                   
                 <p><button onClick={() => this.add()}><img src={Like} alt="Like" /></button>========<button onClick={() => this.min()}><img src={DisLike} alt="DisLike" /></button></p>   
+                
                 <div>
                     <span> { this.state.countLik }</span>============<span> { this.state.countDis }</span>
                 </div>
